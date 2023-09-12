dcf80c75c29d1a7d0f74423b804e8553f23eeba4
Vanja Maric <vm222hx@student.lnu.se>
2023-09-12 21:45:01 +0200

    Resize image

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.awt.image.BufferedImage;
+
 class Main {
   public static void main(String[] args) {
+    ImageLoader imageLoader = new ImageLoader();
+    BufferedImage image = imageLoader.loadImage("src/images/cat.png", 5, 6);
+
     System.out.println("Hello");
+
+    System.out.println(image.getWidth() * image.getHeight());
+   
   }
 }
+
+// Has alfa true?????
